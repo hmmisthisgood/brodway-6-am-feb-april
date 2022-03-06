47e7b0f86ff5187ebc3388375d2cd78f44ce5577
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-03-06 08:27:41 +0545

    added custom back button in post screen and  prevent hardware back button press with WillPopScope

--- firstapp/lib/post_screen.dart
+++ firstapp/lib/post_screen.dart
@@ -7,21 +7,35 @@ class PostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                PostWidget(""),
-                PostWidget(""),
-                PostWidget(""),
-                PostWidget(""),
-              ],
+    return WillPopScope(
+        onWillPop: () {
+          /// true = to allow pop/back
+          /// false = to prevent pop/back
+
+          return Future.value(true);
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios_new),
+                onPressed: () {
+                  // Navigator.pop(context);
+                },
+              ),
             ),
-          ),
-        ));
+            body: Container(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    PostWidget(""),
+                    PostWidget(""),
+                    PostWidget(""),
+                    PostWidget(""),
+                  ],
+                ),
+              ),
+            )));
   }
 }
